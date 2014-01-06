2f17d6d42eff24517fbb47733ce90847cedf51db
JesseHerrick <jessegrantherrick@gmail.com>
2014-01-06 16:53:55 -0500

    Start redesign.

--- lib/jekyll-ftp.rb
+++
@@ -1,9 +0,0 @@
-# 3rd Party
-require 'colorize'
-
-# Local
-require 'jekyll-ftp/clean'
-require 'jekyll-ftp/deploy'
-require 'jekyll-ftp/error'
-require 'jekyll-ftp/send'
-require 'jekyll-ftp/version'
--- lib/jekyll-ftp/version.rb
+++
@@ -1,3 +0,0 @@
-module Jekyll_FTP
-	VERSION = "0.2.0"
-end
